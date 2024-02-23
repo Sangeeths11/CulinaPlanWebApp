605f266e487d1ed3358eca19b8e903c5d42238cc
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2024-02-24 00:26:25 +0100

    index page build with simple example intro

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex flex-col items-center justify-center min-h-screen relative">
+    <div class="absolute top-0 right-0 p-5">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Login
+      </button>
+    </div>
+    <div class="text-center">
+      <h1 class="text-6xl font-bold">
+        Welcome to CulinaPlan
+      </h1>
+      <p class="mt-4 text-xl max-w-lg mx-auto">
+        CulinaPlan is an innovative web application that aims to revolutionize the everyday cooking experience. With a user-friendly interface, CulinaPlan allows users to discover recipes, create personalized meal plans and efficiently generate shopping lists.
+      </p>
+      <button class="bg-green-500 text-white font-bold py-2 px-4 rounded mt-8 effect-button">
+        Start Exploring!
+      </button>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+</script>
+
+<style>
+
+</style>
