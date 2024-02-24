c46b4e4408a8dba9b8a794774dd7443f2748f706
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2024-02-24 01:51:15 +0100

    Create login page layout and add login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,58 @@
 <template>
-  <h1>Login</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div class="bg-white p-6 rounded shadow-md w-full max-w-md">
+      <h1 class="text-2xl md:text-3xl font-bold mb-4 text-center">Login</h1>
+      <div class="flex items-center justify-center mb-6">
+        <img 
+        src="assets/spoon.png"
+        alt="Logo"
+        class="w-16 h-16 rounded-full">
+      </div>
+      <form @submit.prevent="login" class="flex flex-col items-center">
+        <div class="mb-4 w-full">
+          <label 
+          class="block text-gray-700 text-sm font-bold mb-2" 
+          for="email">
+            Email
+          </label>
+          <div class="flex items-center relative">
+            <Icon name="ic:baseline-email" class="absolute left-3 w-5 h-5 text-gray-400" />
+            <input 
+            class="shadow appearance-none border rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+            id="email" 
+            type="email" 
+            placeholder="Email" 
+            v-model="email">
+          </div>
+        </div>
+        <div class="mb-6 w-full">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+          <div class="flex items-center relative">
+            <Icon name="carbon:password" class="absolute left-3 w-5 h-5 text-gray-400" style="top: 40%; transform: translateY(-50%);" />
+            <input 
+            class="shadow appearance-none border rounded w-full py-2 pl-10 pr-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+            id="password" 
+            type="password" 
+            placeholder="******************" 
+            v-model="password">
+          </div>
+        </div>
+        <div class="mb-4 w-full">
+          <LoginSubmitButton/>
+        </div>
+        <div class="flex flex-col items-center md:flex-row w-full justify-center">
+          <span class="text-gray-600 text-sm">
+            Don't have an account?
+          </span>
+          <NuxtLink to="/register" class="text-blue-500 text-sm">
+            Register here
+          </NuxtLink>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup>
 
+<script lang="ts">
 </script>
-
-<style>
-
-</style>
