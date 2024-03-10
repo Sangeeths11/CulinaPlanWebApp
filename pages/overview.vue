f771629d088bb820be1caf1910525e99cf356f4b
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2024-03-10 14:34:35 +0100

    Add side navigation to overview page

--- pages/overview.vue
+++ pages/overview.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+const showSideNav = ref(false);
+
+const toggleSideNav = () => {
+  showSideNav.value = !showSideNav.value;
+};
+
 const router = useRouter();
+
 definePageMeta({
   title: 'Rezepte Übersicht',
   middleware: ['auth-index']
@@ -36,15 +43,35 @@ const openCreateRecipeModal = () => {
 
 <template>
   <div class="min-h-screen bg-gray-100">
-    <div class="p-5 flex justify-between items-center">
-      <button class="p-2 bg-blue-500 text-white rounded-md">Menu</button>
+    <div v-show="showSideNav" class="fixed inset-0 bg-gray-600 bg-opacity-50 z-50" @click="toggleSideNav"></div>
+    
+    <div v-show="showSideNav" class="fixed inset-y-0 left-0 bg-white shadow-md z-50 w-64 flex flex-col py-4 transform transition-transform duration-300" :class="{ '-translate-x-full': !showSideNav }">
+      <router-link to="/dashboard" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
+        <Icon name="material-symbols:dashboard-outline" class="h-6 w-6 text-blue-500" />
+        Dashboard
+      </router-link>
+      <router-link to="/calenderView" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
+        <Icon name="uil:calender" class="h-6 w-6 text-blue-500" />
+        Kalender
+      </router-link>
+      <router-link to="/profile" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
+        <Icon name="iconamoon:profile" class="h-6 w-6 text-blue-500" />
+        Profile
+      </router-link>
+    </div>
+
+    <div class="flex justify-between items-center p-5">
+      <button @click="toggleSideNav" class="p-2 bg-blue-500 text-white rounded-md">
+        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M3 9h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0-4h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 8h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z" clip-rule="evenodd"></path>
+        </svg>
+      </button>
+      <h1 class="text-3xl font-bold">Recipe overview</h1>
       <button class="p-2 bg-blue-500 text-white rounded-md">
         <img src="assets/spoon.png" alt="Profil" class="h-8 w-8 rounded-full">
       </button>
     </div>
-    <div>
-      <h1 class="text-3xl font-bold p-5">Recipe overview</h1>
-    </div>
+
     <div class="p-5">
       <FilterBar/>
     </div>
