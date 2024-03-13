a1b9b97f04a5a53c110d2e9d5370cb2129b1f514
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2024-03-13 21:03:03 +0100

    Refactor recipe selection logic in RecipeModal.vue

--- components/RecipeModal.vue
+++ components/RecipeModal.vue
@@ -16,28 +16,28 @@
           <label for="morgenRecipe" class="block text-sm font-medium text-gray-700 mb-1">Morgen:</label>
           <select v-model="morgenRecipe" id="morgenRecipe" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             <option disabled value="">Bitte auswählen</option>
-            <option v-for="recipe in breakfastRecipes" :key="recipe.id" :value="recipe.name">{{ recipe.name }}</option>
+            <option v-for="recipe in breakfastRecipes" :key="recipe.id" :value="{ id: recipe.id, name: recipe.name }">{{ recipe.name }}</option>
           </select>
         </div>
         <div class="mb-4">
           <label for="lunchRecipe" class="block text-sm font-medium text-gray-700 mb-1">Mittag:</label>
           <select v-model="lunchRecipe" id="lunchRecipe" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             <option disabled value="">Bitte auswählen</option>
-            <option v-for="recipe in lunchRecipes" :key="recipe.id" :value="recipe.name">{{ recipe.name }}</option>
+            <option v-for="recipe in lunchRecipes" :key="recipe.id" :value="{ id: recipe.id, name: recipe.name }">{{ recipe.name }}</option>
           </select>
         </div>
         <div class="mb-4">
           <label for="eveningRecipe" class="block text-sm font-medium text-gray-700 mb-1">Abend:</label>
           <select v-model="eveningRecipe" id="eveningRecipe" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             <option disabled value="">Bitte auswählen</option>
-            <option v-for="recipe in dinnerRecipes" :key="recipe.id" :value="recipe.name">{{ recipe.name }}</option>
+            <option v-for="recipe in dinnerRecipes" :key="recipe.id" :value="{ id: recipe.id, name: recipe.name }">{{ recipe.name }}</option>
           </select>
         </div>
         <div class="mb-4">
           <label for="snackRecipe" class="block text-sm font-medium text-gray-700 mb-1">Snack:</label>
           <select v-model="snackRecipe" id="snackRecipe" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             <option disabled value="">Bitte auswählen</option>
-            <option v-for="recipe in snackRecipes" :key="recipe.id" :value="recipe.name">{{ recipe.name }}</option>
+            <option v-for="recipe in snackRecipes" :key="recipe.id" :value="{ id: recipe.id, name: recipe.name }">{{ recipe.name }}</option>
           </select>
         </div>
         <div class="flex justify-end">
@@ -58,10 +58,11 @@ import { ref, onMounted } from 'vue';
 const supabase = useSupabaseClient()
 const emits = defineEmits(['close', 'assign']);
 const selectedDate = ref('');
-const morgenRecipe = ref('');
-const lunchRecipe = ref('');
-const eveningRecipe = ref('');
-const snackRecipe = ref('');
+const morgenRecipe = ref<{ id: string; name: string } | null>(null);
+const lunchRecipe = ref<{ id: string; name: string } | null>(null);
+const eveningRecipe = ref<{ id: string; name: string } | null>(null);
+const snackRecipe = ref<{ id: string; name: string } | null>(null);
+
 
 
 const breakfastRecipes = ref([]);
@@ -76,20 +77,22 @@ const closeModal  = () => {
 const assignRecipe = () => {
   if (selectedDate.value && morgenRecipe.value && lunchRecipe.value && eveningRecipe.value && snackRecipe.value) {
 
-    const assignvalues = [];
-    assignvalues.push(selectedDate.value);
-    assignvalues.push(morgenRecipe.value);
-    assignvalues.push(lunchRecipe.value);
-    assignvalues.push(eveningRecipe.value);
-    assignvalues.push(snackRecipe.value);
-    // console.log(assignvalues);
+    const assignValues = {
+      date: selectedDate.value,
+      morgen: morgenRecipe.value,
+      lunch: lunchRecipe.value,
+      evening: eveningRecipe.value,
+      snack: snackRecipe.value,
+    };
     
-    emits('assign', assignvalues);
+    emits('assign', assignValues);
+
+    // Resetting the form fields
     selectedDate.value = '';
-    morgenRecipe.value = '';
-    lunchRecipe.value = '';
-    eveningRecipe.value = '';
-    snackRecipe.value = '';
+    morgenRecipe.value = null;
+    lunchRecipe.value = null;
+    eveningRecipe.value = null;
+    snackRecipe.value = null;
   }
 };
 
