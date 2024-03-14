d6426a41bb30cda821cb3f26e8c82f600aa3ecdd
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2024-03-14 18:15:05 +0100

    Refactor overview page layout and remove unused code

--- pages/overview.vue
+++ pages/overview.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-const showSideNav = ref(false);
 const supabase = useSupabaseClient()
 const recipeStore = useRecipeStore();
 const showDeleteButton = ref(false);
 const showEditButton = ref(false);
 
-
-
-const toggleSideNav = () => {
-  showSideNav.value = !showSideNav.value;
-};
-
 const router = useRouter();
 
 definePageMeta({
   title: 'Rezepte Übersicht',
-  middleware: ['auth-index']
+  middleware: ['auth-index'],
+  layout: 'default',
 });
 
 onMounted(() => {
@@ -62,40 +56,11 @@ const applyFilter = (filterData) => {
   console.log('Filter angewendet:', filterData);
   recipeStore.fetchRecipes(filterData);
 };
-definePageMeta({
-  layout: 'non',
-})
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-100">
-    <div v-show="showSideNav" class="fixed inset-0 bg-gray-600 bg-opacity-50 z-50" @click="toggleSideNav"></div>
-    
-    <div v-show="showSideNav" class="fixed inset-y-0 left-0 bg-white shadow-md z-50 w-64 flex flex-col py-4 transform transition-transform duration-300" :class="{ '-translate-x-full': !showSideNav }">
-      <router-link to="/dashboard" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
-        <Icon name="material-symbols:dashboard-outline" class="h-6 w-6 text-blue-500" />
-        Dashboard
-      </router-link>
-      <router-link to="/calenderView" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
-        <Icon name="uil:calender" class="h-6 w-6 text-blue-500" />
-        Kalender
-      </router-link>
-      <router-link to="/profile" class="px-6 py-3 text-lg font-semibold flex items-center space-x-4 hover:bg-gray-100">
-        <Icon name="iconamoon:profile" class="h-6 w-6 text-blue-500" />
-        Profile
-      </router-link>
-    </div>
 
-    <div class="flex justify-between items-center p-5">
-      <button @click="toggleSideNav" class="p-2 bg-blue-500 text-white rounded-md">
-        <Icon name="heroicons-solid:menu" class="h-6 w-6" />
-      </button>
-      
-      <div class="flex items-center space-x-2">
-        <span class="text-gray-600">Benutzername</span>
-        <img src="assets/spoon.png" alt="Profil" class="h-10 w-10 rounded-full">
-      </div>
-    </div>
     <div class="p-5">
       <h1 class="text-3xl font-bold mb-2">Rezepte Übersicht</h1>
     </div>
